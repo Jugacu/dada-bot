4ace2781097993445f80caa4fb328c0c2a98d6bc
Julián Sánchez <julian@jugacu.es>
2020-07-13 00:27:58 +0200

    removed dist folder

--- dist/discord/events/Message.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Event_1 = require("../Event");
-class Message extends Event_1.Event {
-    exec(data) {
-        return true;
-    }
-}
-exports.default = Message;
